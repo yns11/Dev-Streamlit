7ada32fd97c0ee3b9b8ce4c37a1f5e1e94c7d102
yns11 <younes.elhachi@gmail.com>
2026-07-15 01:46:12 +0200

    Delete src directory

--- src/app_creation/bl_core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Coeur métier partagé entre les deux applications BL dématérialisés.
-
-Ce package est la copie synchronisée de shared/bl_core (source de vérité à la
-racine du dépôt). Ne pas l'éditer directement dans les dossiers d'app :
-modifier shared/bl_core puis exécuter tools/sync_shared.ps1.
-"""
